6d62383b1025ed020db2caf24fe813d03a1e3d5d
Jmirza2 <159500782+Jmirza2@users.noreply.github.com>
2024-02-08 19:49:19 -0600

    Add files via upload

---
+++ PS4P1.py
@@ -0,0 +1,8 @@
+examScore_1 = input("Enter your first exam score: ")
+examScore_2 = input("Enter your second exam score: ")
+
+total_1 = 0.6 * float(examScore_1)
+total_2 = 0.4 * float(examScore_2)
+
+print("The first exam is worth: ", total_1)
+print("The second exam is worth: ", total_2)
