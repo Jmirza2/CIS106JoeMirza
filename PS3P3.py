f259a0ba3e090398f597bfb334069a03f837bf53
Jmirza2 <159500782+Jmirza2@users.noreply.github.com>
2024-02-10 22:51:15 -0600

    Add files via upload

---
+++ PS3P3.py
@@ -0,0 +1,24 @@
+#15%
+meal_1 = float(input("Enter the total cost of meal 1: " )) # 10
+tip = meal_1 * 0.15 # 1.5
+total_with_tip = meal_1 + tip
+print("Total: ", meal_1)
+print("Tip: ", tip)
+print("Total with tip: ", total_with_tip) # 11.5
+
+#18%
+meal_2 = float(input("Enter the total cost of meal 2: " )) # 10
+tip = meal_2 * 0.18 # 1.8
+total_with_tip = meal_2 + tip
+print( "Total: ", meal_2)
+print("Tip: ", tip)
+print("Total with tip: ", total_with_tip) # 11.8 
+ 
+#20%
+meal_3 = float(input("Enter the total cost of meal 3: " )) # 10
+tip = meal_3 * 0.20 # 2.0
+total_with_tip = meal_3 + tip # 12.0
+print("Total: ", meal_3)
+print("Tip: ", tip)
+print("Total with tip: ", total_with_tip)
+
