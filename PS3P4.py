ec7287a4991bc41e565b76b3c6cf29e857b6ab89
Jmirza2 <159500782+Jmirza2@users.noreply.github.com>
2024-02-10 21:15:57 -0600

    Add files via upload

---
+++ PS3P4.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first name: ")
+steps = float(input("Enter the number of steps you have walked today:"))
+
+total_steps = steps * 0.25
+
+print(first_name, "burned ",total_steps , "calories today")
