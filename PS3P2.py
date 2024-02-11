02ca0ef2509164b8b686990172f1fd5f125ba249
Jmirza2 <159500782+Jmirza2@users.noreply.github.com>
2024-02-11 16:57:33 -0600

    Update PS3P2.py

--- PS3P2.py
+++ PS3P2.py
@@ -1,8 +1,7 @@
-price_per_share = int(input("Enter Price Per Share: "))
-current_Stock = int(input("Enter Current Stock Price: "))
-quantity = int(input("Enter Quantity: "))
+price_per_share = float(input("Enter Price Per Share: "))
+current_Stock = float(input("Enter Current Stock Price: "))
+quantity = float(input("Enter Quantity: "))
 
 change_in_value = (current_Stock - price_per_share) * quantity
 
 print("Change in value totals to : ", change_in_value)
-
