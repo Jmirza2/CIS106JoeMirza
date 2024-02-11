5ff59bad1d8d904e786ec2caa1e68bbd2eb0d04f
Jmirza2 <159500782+Jmirza2@users.noreply.github.com>
2024-02-11 14:35:50 -0600

    Update PS3P1.py

--- PS3P1.py
+++ PS3P1.py
@@ -1,8 +1,11 @@
-examScore_1 = int(input("Enter your first exam score: "))
-examScore_2 = int(input("Enter your second exam score: "))
+examScore_1 = input("Enter your first exam score: ")
+examScore_2 = input("Enter your second exam score: ")
 
 total_1 = 0.6 * float(examScore_1)
 total_2 = 0.4 * float(examScore_2)
 
 print("The first exam is worth: ", total_1)
 print("The second exam is worth: ", total_2)
+
+total_score = total_1 + total_2
+print("Your total score is: ", total_score)
