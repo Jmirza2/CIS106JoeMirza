921875fdf7d15f39ca5bfed91790cee4e427b4f7
Jmirza2 <159500782+Jmirza2@users.noreply.github.com>
2024-02-17 20:11:59 -0600

    Update PS5P5.py

--- PS5P5.py
+++ PS5P5.py
@@ -1,21 +1,21 @@
 lastName = input("Enter your last name: ")
-nmbrDept = int(input("Enter the number of dependents: "))
+nmbrDept = input("Enter the number of dependents: ")
 grossIncome = int(input("Enter your gross income: "))
 
-adjGrossPay = grossIncome - nmbrDept * 12000.00
+adjGrossPay = grossIncome - 12000.00 * float(nmbrDept)
 
 if adjGrossPay > 50000.00:
-  taxRate = 0.20
+  taxRate = adjGrossPay * 0.20
 else:
-  taxRate = 0.10
+  taxRate = adjGrossPay * 0.10
 
-incomeTax = adjGrossPay * taxRate
+# incomeTax = adjGrossPay * taxRate
 
-if incomeTax < 0:  
-  incomeTax = 100.00
+if taxRate < 0:  
+  taxRate = 100.00 
 
 print("Last name: " + lastName)
 print('Gross income: ', grossIncome)
 print('Number of dependents: ', nmbrDept)
 print('Adjusted gross income: ', adjGrossPay)
-print('Income tax: ', incomeTax)
+print('Income tax: ', taxRate)
