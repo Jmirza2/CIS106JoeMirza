7f960bb5f2cdfa952aca8a8dd0cc98c262a7caa1
Jmirza2 <159500782+Jmirza2@users.noreply.github.com>
2024-02-25 20:15:37 -0600

    Update PS6P2.py

--- PS6P2.py
+++ PS6P2.py
@@ -1,11 +1,11 @@
-part = input("Enter a part number: ")
+part = int(input("Enter a part number: "))
 quantity = int(input("Enter a quantity: "))
 
-if part == "10" or part == "55":
+if part == 10 or part == 55:
   unit_Cost = 1.00
-elif part == "99":
+elif part == 99:
   unit_Cost = 2.00
-elif part == "80" or part == "70":
+elif part == 80 or part == 70:
   unit_Cost = 3.00
 else:
   unit_Cost = 5.00
